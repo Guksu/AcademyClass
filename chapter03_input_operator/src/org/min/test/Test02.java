4607964bf8d0d93ef4e8b5d8c1fcd33c5efa5ade
Guksu <87972252+Guksu@users.noreply.github.com>
2021-08-12 00:11:35 +0900

    Add files via upload

--- chapter03_input_operator/src/org/min/test/Test02.java
+++ chapter03_input_operator/src/org/min/test/Test02.java
@@ -1,10 +1,21 @@
 package org.min.test;
 
+import java.util.Scanner;
+
 public class Test02 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Q. 나이를 입력받아 "성인" 또는 "미성년자" 출력
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("나이를 입력하시오 >>");
+		if( sc.nextInt() >19) {
+			System.out.println("성인");
+		}
+		else {
+			System.out.println("미성년자");
+		}
+		sc.close();
+		
 	}
-
 }
---
+++ chapter03_input_operator/src/org/min/test/Test03.java
@@ -0,0 +1,9 @@
+package org.min.test;
+
+public class Test03 {
+	public static void main(String[] args) {
+
+
+		
+	}
+}
